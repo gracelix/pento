d5bef3f94a84947faa7a09cd7f4afc11d9914526
gracelix <65653346+gracelix@users.noreply.github.com>
2023-01-13 17:11:10 +0800

    add untracked files

---
+++ lib/pento_web/live/wrong_live.ex
@@ -0,0 +1,83 @@
+defmodule PentoWeb.WrongLive do
+  use Phoenix.LiveView, layout: {PentoWeb.LayoutView, "live.html"}
+  
+  def mount(_params, session, socket) do
+    {:ok,
+     assign(socket,
+       score: 0,
+       message: "Make a guess:",
+       answer: Enum.random([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) |> to_string(),
+       is_correct: false,
+       session_id: session["live_socket_id"]
+     )}
+  end
+
+  def render(assigns) do
+    ~H"""
+      <h1>Your score: <%= @score %></h1>
+      <h2>
+        <%= @message %>
+        <%!-- <%= if @is_correct do%>
+          <%= live_redirect "Play again!", to: Routes.live_path(@socket, PentoWeb.WrongLive) %>
+        <% end %> --%>
+
+      </h2>
+      <h2>
+        <%= for n <- 1..10 do %>
+          <a href="#" phx-click="guess" phx-value-number={n} ><%= n %></a>
+        <% end %>
+        <pre>
+          <%= @current_user.email %>
+          <%= @session_id %>
+        </pre>
+    </h2>
+    """
+  end
+
+  def time(), do: DateTime.utc_now() |> to_string
+
+  # handle_event/3: handle events sent by the client (ie. mouse click)
+  #   1) message name
+  #   2) map with metadata related to the event
+  #   3) state for the live view (socket)
+
+  def handle_event("guess", %{"number" => guess}, socket) do
+    {msg, score, is_correct} = socket |> check_answer(guess)
+
+    {
+      :noreply,
+      socket
+      |> assign(
+        message: msg,
+        score: score,
+        is_correct: is_correct
+      )
+    }
+  end
+
+  defp check_answer(%{assigns: %{answer: answer}} = socket, guess) do
+    case answer do
+      ^guess -> {"Correct answer: #{guess}. Congratulations!", socket.assigns.score + 1, true}
+      ^answer -> {"Your answer: #{guess}. Wrong! Try again.", socket.assigns.score - 1, false}
+    end
+  end
+end
+
+# PRINT TO CONSOLE
+# IO.inspect socket.assigns.answer, label: "answer"
+
+# ALTERNATE METHOD -- PATTERN MATCHING answer WITH guess
+# def handle_event("guess", %{"number" => guess}, %{assigns: %{answer: answer}} = socket) do
+#   message = "Your guess: #{guess}. Wrong! Guess again."
+#   score = socket.assigns.score - 1
+
+#   {
+#     :noreply,
+#     assign(
+#       socket,
+#       message: message,
+#       score: score
+#     )
+#   }
+# end
+# %{assigns: %{answer: answer, score: score}} = socket
---
+++ test/support/fixtures/accounts_fixtures.ex
@@ -0,0 +1,43 @@
+defmodule Pento.AccountsFixtures do
+  @moduledoc """
+  This module defines test helpers for creating
+  entities via the `Pento.Accounts` context.
+  """
+
+  def unique_user_email, do: "user#{System.unique_integer()}@example.com"
+  def valid_user_password, do: "hello world!"
+
+  def valid_user_attributes(attrs \\ %{}) do
+    Enum.into(attrs, %{
+      email: unique_user_email(),
+      password: valid_user_password()
+    })
+  end
+
+  @spec user_fixture(any) :: any
+  def user_fixture(attrs \\ %{}, opts \\ []) do
+    {:ok, user} =
+      attrs
+      |> valid_user_attributes()
+      |> Pento.Accounts.register_user()
+
+    # if account is confirmed,
+    if Keyword.get(opts, :confirmed, true), do: Pento.Repo.transaction(confirm_user_multi(user))
+    user
+  end
+
+  def extract_user_token(fun) do
+    {:ok, captured_email} = fun.(&"[TOKEN]#{&1}[TOKEN]")
+    [_, token | _] = String.split(captured_email.text_body, "[TOKEN]")
+    token
+  end
+
+  defp confirm_user_multi(user) do
+    Ecto.Multi.new()
+    |> Ecto.Multi.update(:user, Pento.Accounts.User.confirm_changeset(user))
+    |> Ecto.Multi.delete_all(
+      :tokens,
+      Pento.Accounts.UserToken.user_and_contexts_query(user, ["confirm"])
+    )
+  end
+end
