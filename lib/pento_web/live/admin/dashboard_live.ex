13f1f3febbafefcd06b7bb4836d988a682f8be21
gracelix <65653346+gracelix@users.noreply.github.com>
2023-02-02 11:11:09 +0800

    ch9: live user activity

--- lib/pento_web/live/admin/dashboard_live.ex
+++ lib/pento_web/live/admin/dashboard_live.ex
@@ -1,23 +1,36 @@
 defmodule PentoWeb.Admin.DashboardLive do
   use PentoWeb, :live_view
+  alias PentoWeb.Admin.UserActivityLive
   alias PentoWeb.Admin.SurveyResultsLive
   alias PentoWeb.Endpoint
 
   @survey_results_topic "survey_results"
+  @user_activity_topic "user_activity"
 
   def mount(_params, _session, socket) do
     # connected?/1 fn -> only call subscribe/1 if the socket is connected (during 2nd mount/3 call)
     if connected?(socket) do
       Endpoint.subscribe(@survey_results_topic)
+      Endpoint.subscribe(@user_activity_topic)
     end
 
-    {:ok, socket |> assign(:survey_results_component_id, "survey-results")}
+    {:ok,
+     socket
+     |> assign(:survey_results_component_id, "survey-results")
+     |> assign(:user_activity_component_id, "user-activity")}
   end
 
   # receive the message broadcasted over thecommon topic and respond
   def handle_info(%{event: "rating_created"}, socket) do
     # use send_update/3 to send message from parent live view to child component asynchronously
+    # tells the SurveyResultsLive component to invoke its update/2 function
     send_update(SurveyResultsLive, id: socket.assigns.survey_results_component_id)
+    {:noreply, socket}
+  end
+
+  def handle_info(%{event: "presence_diff"}, socket) do
+    # tells the SurveyResultsLive component to invoke its update/2 function
+    send_update(UserActivityLive, id: socket.assigns.user_activity_component_id)
     {:noreply, socket}
   end
 end
--- lib/pento_web/live/product_live/show.ex
+++ lib/pento_web/live/product_live/show.ex
@@ -2,23 +2,36 @@ defmodule PentoWeb.ProductLive.Show do
   use PentoWeb, :live_view
 
   alias Pento.Catalog
+  alias PentoWeb.Presence
+  alias Pento.Accounts
 
   @impl true
   def mount(_params, _session, socket) do
-    {
-      :ok,
-      socket
-      #  |> assign(:greeting, "Welcome to Pento!")
-    }
+    {:ok, socket}
   end
 
-  @impl true
+  # look up the product then conditionally track the user's presence
   def handle_params(%{"id" => id}, _, socket) do
+    product = Catalog.get_product!(id)
+    maybe_track_user(product, socket)
+
     {:noreply,
      socket
      |> assign(:page_title, page_title(socket.assigns.live_action))
      |> assign(:product, Catalog.get_product!(id))}
   end
+
+  # only do user presence tracking if the live view is loading with :show (instead of :edit)
+  def maybe_track_user(
+        product,
+        %{assigns: %{live_action: :show, current_user: current_user}} = socket
+      ) do
+    if connected?(socket) do
+      Presence.track_user(self(), product, current_user.email)
+    end
+  end
+
+  def maybe_track_user(_product, _socket), do: nil
 
   defp page_title(:show), do: "Show Product"
   defp page_title(:edit), do: "Edit Product"
